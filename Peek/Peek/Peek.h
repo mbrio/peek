2429421952eb80ce0dc905f7bb1b310d93528af0
Michael Diolosa <michael.diolosa@gmail.com>
2012-08-08 21:27:51 -0400

    Added a single header for library

---
+++ Peek/Peek/Peek.h
@@ -0,0 +1,10 @@
+//
+//  Peek.h
+//  Peek
+//
+//  Created by Michael Diolosa on 8/8/12.
+//  Copyright (c) 2012 Michael Diolosa (@mbrio)
+//  Peek may be freely distributed under the MIT license.
+//
+
+#import "PKPeekViewController.h"
